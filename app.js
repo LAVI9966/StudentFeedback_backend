466b68ab6d4b1b25299f2f573350b422d9b79335
lavish <lavishgehlod210204@acropolis.in>
2024-04-20 22:05:25 -0700

    msg"

--- app.js
+++ app.js
@@ -10,10 +10,7 @@ import adminrouter from "./routers/admin.js";
 import facultyauth from "./routers/faculty.js";
 import studentauth from "./routers/student.js";
 import Jwt from "jsonwebtoken";
-import { sendmail } from "./sendmail.js";
-import { sendmailfaculty } from "./sendmail.js";
 
-const __filename = fileURLToPath(import.meta.url);
 const app = express();
 app.use(authrouter);
 app.use(adminrouter);
@@ -25,25 +22,12 @@ app.use(
   })
 );
 
-app.post("/sendmail", sendmail);
-app.post("/sendmailfaculty", sendmailfaculty);
-
 // Get the directory name of the current module
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+// const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // app.get("/", (req, res) => {
 //   app.use(express.static(path.resolve(__dirname, "frontend", "dist")));
 //   res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-// });
-// app.get("/login", (req, res) => {
-//   app.use(express.static(path.resolve(__dirname, "frontend", "dist")));
-//   res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-// });
-
-// app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "/frontend/dist/index.html"));
 // });
 
 app.listen(process.env.PORT, () => {
